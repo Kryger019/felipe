fa7e1755c401244a43330a14ee9d0d387172f729
Kryger019 <160683097+Kryger019@users.noreply.github.com>
2024-06-19 20:40:58 -0300

    felipe

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,15 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div className="container">
-      <h1>👕🐀Ratinho Kryger the Best Player in Antígua & Barbuda🐀👕</h1>
+      <h1 className={styles.titulo}>👕🐀Ratinho Kryger the Best Player in Antígua & Barbuda🐀👕</h1>
+    <Image
+    src="/Rato.jfif"
+    width={500}
+    height={1212}
+    alt="Rato"
+    className={styles.imagem}
+    />
+
     </div>
   );
 }
---
+++ src/page.jsx
@@ -0,0 +1,12 @@
+import Botao from "@/components/botao";
+
+export default function Login(){
+    return(
+        <div className="container">
+            <h1>Tela de Login</h1>
+
+            <Botao txtBotao={'Acessar o Sistema'}/>
+
+        </div>
+    )
+} 
